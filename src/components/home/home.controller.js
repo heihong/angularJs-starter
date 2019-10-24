98ddc3a022e1b080f8412825f81aae7e266a44e6
htraing <traing.hh@sfeir.com>
2019-10-24 19:06:15 +0200

    start

--- src/components/home/home.controller.js
+++ src/components/home/home.controller.js
@@ -1,12 +1,9 @@
 export default class HomeController {
 	constructor($log) {
 		'ngInject';
-
-		this.$log = $log;
 	}
 
 	$onInit = () => {
 		this.heading = 'Welcome to AngularJS ES6 Starter-Kit';
-		this.$log.info('Activated Home View.');
 	};
 }
